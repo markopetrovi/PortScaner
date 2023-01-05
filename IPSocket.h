7c2ac6e0136c99865ef24aacf1fb0bd14b85587f
Marko Petrovic <88789103+markopetrovi@users.noreply.github.com>
2023-01-05 02:29:24 +0100

    Add files via upload

---
+++ IPSocket.h
@@ -0,0 +1,105 @@
+#ifndef IP_SOCKET_H
+#define IP_SOCKET_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdbool.h>
+
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+/*
+Description:
+	Printf()	- Normal printf() function, but with added newline at the end of writing and stdout buffer flushing
+	Rest functions are equivalent of normal syscalls, but with error handling:
+	Write(2)	- write(2) syscall sa ispisivanjem poruke o gresci na stdout ako do nje dodje.
+	Bind(2)		- bind(2) syscall sa ispisivanjem poruke o gresci na stdout ako do nje dodje.
+	Listen(2)	- listen(2) syscall sa ispisivanjem poruke o gresci na stdout ako do nje dodje.
+	Accept(2)	- accept(2) syscall sa ispisivanjem poruke o gresci na stdout ako do nje dodje.
+	Connect(2)	- connect(2) syscall sa ispisivanjem poruke o gresci na stdout ako do nje dodje.
+*/
+int Connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
+{
+	int stat = connect(sockfd, addr, addrlen);
+	if(stat == -1)
+	{
+		printf("%s", "connect(");
+		printf("%d", sockfd);
+		printf("%s", ", ...) FAILED: ");
+		printf("%d", errno);
+		printf("%s", "\n");
+		fflush(stdout);
+	}
+	return stat;
+}
+int Accept(int sockfd, struct sockaddr *restrict addr, socklen_t *restrict addrlen)
+{
+	int fd = accept(sockfd, addr, addrlen);
+	if(fd == -1)
+	{
+		printf("%s", "accept(");
+		printf("%d", sockfd);
+		printf("%s", ", ...) FAILED: ");
+		printf("%d", errno);
+		printf("%s", "\n");
+		fflush(stdout);
+	}
+	return fd;
+}
+int Write(int fd, const void *buf, size_t count)
+{
+	int stat = write(fd, buf, count);
+	if (stat == -1)
+	{
+		printf("%s", "write(");
+		printf("%d", fd);
+		printf("%s", ", ");
+		printf("%s", buf);
+		printf("%s", ", ");
+		printf("%d", count);
+		printf("%s", ") FAILED: ");
+		printf("%d", errno);
+		printf("%s", "\n");
+		fflush(stdout);
+	}
+}
+int Printf(void *data)
+{
+	int num = printf("%s", data);
+	printf("%s", "\n");
+	fflush(stdout);
+	return num;
+}
+int Bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
+{
+	int stat = bind(sockfd, addr, addrlen);
+	if(stat == -1)
+	{
+		printf("%s", "bind(");
+		printf("%d", sockfd);
+		printf("%s", ", ...) FAILED: ");
+		printf("%d", errno);
+		printf("%s", "\n");
+		fflush(stdout);
+	}
+}
+int Listen(int sockfd, int backlog)
+{
+	int stat = listen(sockfd, backlog);
+	if (stat == -1)
+	{
+		printf("%s", "listen(");
+		printf("%d", sockfd);
+		printf("%s", ", ");
+		printf("%d", backlog);
+		printf("%s", ") FAILED: ");
+		printf("%d", errno);
+		printf("%s", "\n");
+		fflush(stdout);
+	}
+	return stat;
+}
+
+#endif
---
+++ client.c
@@ -0,0 +1,83 @@
+/* Copyright (C) Marko Petrović 2022
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or 
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with this program. 
+* If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "IPSocket.h"
+#include <signal.h>
+
+int tcp_socket;
+void handler(int signum)
+{
+	close(tcp_socket);
+	tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
+}
+int main()
+{
+	struct sigaction sig;
+	sig.sa_handler = &handler;
+	sigaction(SIGALRM, (const struct sigaction *restrict) &sig, NULL);
+	
+	char ipaddress[16];
+	unsigned int port;
+	
+	Printf("Enter IP address:");
+	read(0, ipaddress, 16);
+	Printf("Enter starting port:");
+	scanf("%u", &port);
+	
+	struct sockaddr_in addr;
+	inet_aton(ipaddress, (struct in_addr *) &addr.sin_addr);
+	addr.sin_port = htons(port);
+	addr.sin_family = AF_INET;
+	
+	tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
+	socklen_t addrlen = sizeof(addr);
+	
+	int stat = -1;
+	while(stat != 0 && port < 65536)
+	{
+		port++;
+		addr.sin_port = htons(port);
+		alarm(1);
+		stat = Connect(tcp_socket, (struct sockaddr *) &addr, addrlen);
+		alarm(0);
+	}
+	alarm(0);
+	printf("%d%s", port, "\n");
+	//close(tcp_socket);
+	//return 0;
+	
+	//Connect(tcp_socket, (struct sockaddr *) &addr, addrlen);
+	fcntl(tcp_socket, F_SETFL, O_NONBLOCK);
+	char c[100];
+	Printf("Start!");
+	
+	int count, i = 1;
+	while(true)
+	{
+		count = read(0, c, 100);
+		Write(tcp_socket, c, count);
+		
+		printf("%d", i);
+		Printf(". packet sent");
+		
+		count = read(tcp_socket, c, 100);
+		Write(1, c, count);
+		i++;
+	}
+	
+	close(tcp_socket);
+	
+	return 0;
+}
