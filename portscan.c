adcaea8466b837c21c74f601847fbecd55d9983a
Marko Petrovic <88789103+markopetrovi@users.noreply.github.com>
2023-01-05 02:30:55 +0100

    Update and rename client.c to portscan.c

--- portscan.c
+++ portscan.c
@@ -1,4 +1,4 @@
-/* Copyright (C) Marko Petrović 2022
+/* Copyright (C) Marko Petrovic 2023
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
